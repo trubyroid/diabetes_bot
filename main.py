4fcf1f78e86d0bcff6d60881672a55afc58b2169
Alexander Pikhenko <A_Pikhenko@outlook.com>
2023-09-03 02:37:43 +0300

    создал отдельные функции с клавиатурами
    написал функция для изменения профиля и анкетирования

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import telebot
 import config
-import requests
 from telebot import types
-from registerPageUtils import connect_db, process_name_step
+from src.registerPageUtils import connect_db, process_name_step, handler_questionnaire
+from src.keyboards import main_menu_kb, education_kb, choose_platform_kb, about_school_kb, profile_kb
 
 bot = telebot.TeleBot(config.token)
 
@@ -16,15 +16,15 @@ def start(message):
 
 
 @bot.message_handler(commands=['clear_db'])
-def clear_db(messege):
+def clear_db(message):
     db = connect_db("users.db")
     db.clear_database()
 
 @bot.message_handler(commands=['delete_user'])
-def delete_user(messege):
+def delete_user(message):
     db = connect_db("users.db")
-    bot.send_message(messege.chat.id, 'Введите id пользователя, которого нужно удалить:')
-    db.clear_database()
+    bot.send_message(message.chat.id, 'Введите id пользователя, которого нужно удалить:')
+    bot.register_next_step_handler(message, lambda message: db.delete_user(message.text) )
 
 @bot.message_handler(commands=['all'])
 def view_all(messege):
@@ -38,7 +38,7 @@ def view_all(messege):
 def handle_register(message):
     if message.text == "Зарегистрироваться":
         chat_id = message.chat.id
-        users = {}
+        users = {message.chat.id: {}}
         db = connect_db("users.db")
         # Запрашиваем имя
         bot.send_message(chat_id, 'Введите ваше имя:', reply_markup=types.ReplyKeyboardRemove())
@@ -67,62 +67,28 @@ def get_text_messages(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
     # Главное меню
-    # done
     if message.text == 'Главное меню':
-        btn_education = types.KeyboardButton("Обучение")
-        btn_profile = types.KeyboardButton("Профиль")
-        btn_social_media = types.KeyboardButton('Об онлайн-школе')
-        markup.add(btn_education, btn_profile, btn_social_media)
-        bot.send_message(message.from_user.id, '👀 Выберите вариант из списка.', reply_markup=markup)
+        main_menu_kb(message, markup, bot)
 
     # Обучение
-    # done
     elif message.text == 'Обучение':
-        # keyboard
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn_choose = types.KeyboardButton('Выбрать платформу')
-        btn_get_homework = types.KeyboardButton('Получить домашнее задание')
-        btn_pass_homework = types.KeyboardButton('Сдать домашнее задание')
-        btn_progress = types.KeyboardButton('Прогресс')               # in future
-        btn_need_help = types.KeyboardButton('Нужна помощь')
-        btn_main_menu = types.KeyboardButton('Главное меню')
-        markup.add(btn_choose, btn_get_homework, btn_pass_homework, btn_progress, btn_need_help, btn_main_menu)
-        # messege
-        bot.send_message(message.from_user.id, '👀 Выберите вариант из списка.', reply_markup=markup)
+        education_kb(message, markup, bot)
 
     # Выбрать платформу
     # in progress
     elif message.text == 'Выбрать платформу':
-        # keyboard
-        markup = types.InlineKeyboardMarkup()
-        btn_vk_edu = types.InlineKeyboardButton("Школа диабета VK", url='https://vk.com/video/@shkola.diabeta?section=playlists')
-        btn_youtube_edu = types.InlineKeyboardButton("Школа диабета Youtube", url='https://www.youtube.com/playlist?list=PL_dK9vVNI4Vj10OHq4e9pDTqHxpijcTi-')
-        btn_dzen_edu = types.InlineKeyboardButton("Школа диабета Dzen", url='https://dzen.ru/dibet')
-        markup.add(btn_vk_edu)
-        markup.add(btn_youtube_edu)
-        markup.add(btn_dzen_edu)
-        bot.send_message(message.chat.id, "Программа обучения: https://vk.com/@shkola.diabeta-programma\nНажми на кнопку и перейди на сайт".format(message.from_user), reply_markup=markup)
-        # keyboard
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn_main_menu = types.KeyboardButton('Главное меню')
-        markup.add(btn_main_menu)
-        # messege 
-        bot.send_message(message.from_user.id, 'Чтобы вернуться назад нажмите - /back', reply_markup=markup)
+        choose_platform_kb(message, markup, bot)
 
     # Об онлайн-школе
     # in progress
     elif message.text == 'Об онлайн-школе':
-        btn_main_menu = types.KeyboardButton('Главное меню')
-        markup.add(btn_main_menu)
-        bot.send_message(message.from_user.id, config.online_school_description, reply_markup=markup)
+        about_school_kb(message, markup, bot)
 
     elif message.text == 'Профиль':
-        btn_show_profile = types.KeyboardButton('Посмотреть профиль')
-        btn_edit_profile = types.KeyboardButton('Редактировать профиль')
-        btn_get_back = types.KeyboardButton('Главное меню')
-        markup.add(btn_show_profile, btn_edit_profile, btn_get_back)
+        profile_kb(message, markup, bot)
+    elif message.text == 'Редактировать профиль':
+        bot.register_next_step_handler(message, lambda message: handler_questionnaire(message, bot))
 
-        bot.send_message(message.from_user.id, config.profile_section, reply_markup=markup)
     # Некорректный ввод
     # done
     else:
--- src/registerPageUtils.py
+++ src/registerPageUtils.py
@@ -1,23 +1,23 @@
-from users import Database, connect_db
+from src.users import Database, connect_db
 from telebot import types
+from src.keyboards import main_menu_kb
+from src.profile_edit import edit_age
 
 
 def handler_questionnaire(message, bot):
-    pass
+    bot.send_message(message.chat.id, 'Сколько вам лет?', reply_markup=types.ReplyKeyboardRemove())
+    users = {message.chat.id: {}}
+    db = connect_db("users.db")
+    bot.register_next_step_handler(message, lambda message: edit_age(message, users, db, bot))
+
 
 def questionnaire(message, bot):
     if message.text == "Да":
-        bot.register_next_step_handler(message, lambda messege: handler_questionnaire(messege, bot))
+        bot.register_next_step_handler(message, lambda message: handler_questionnaire(message, bot))
     elif message.text == "Нет":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn_education = types.KeyboardButton("Пройти обучение")
-        btn_profile = types.KeyboardButton("Профиль")
-        btn_social_media = types.KeyboardButton('Об онлайн-школе')
-        btn_main_menu = types.KeyboardButton('Главное меню')
-        markup.add(btn_education, btn_profile, btn_social_media, btn_main_menu)
-        bot.send_message(message.from_user.id, '👀 Выберите вариант из списка.', reply_markup=markup)
+        main_menu_kb(message, types.ReplyKeyboardMarkup(resize_keyboard=True), bot)
     else:
-        bot.register_next_step_handler(message, lambda messege: questionnaire(messege, bot))
+        bot.register_next_step_handler(message, lambda message: questionnaire(message, bot))
 
 
 def process_name_step(message, users, db, bot):
@@ -26,7 +26,7 @@ def process_name_step(message, users, db, bot):
 
     # Запрашиваем фамилию
     bot.send_message(chat_id, 'Введите вашу фамилию:')
-    bot.register_next_step_handler(message, lambda messege: process_surname_step(messege, users, db, bot))
+    bot.register_next_step_handler(message, lambda message: process_surname_step(message, users, db, bot))
 
     # Сохраняем имя
     users[chat_id] = {'name': name}
@@ -37,7 +37,7 @@ def process_surname_step(message, users, db, bot):
 
     # Запрашиваем почту
     bot.send_message(chat_id, 'Введите вашу почту:')
-    bot.register_next_step_handler(message, lambda messege: process_email_step(messege, users, db, bot))
+    bot.register_next_step_handler(message, lambda message: process_email_step(message, users, db, bot))
 
     # Добавляем фамилию
     users[chat_id]['surname'] = surname
@@ -60,4 +60,4 @@ def process_email_step(message, users, db, bot):
     btn_no = types.KeyboardButton('Нет')
     markup.add(btn_yes, btn_no)
     bot.send_message(chat_id, 'Для составления персональной программы обучения нам нужно задать вам несколько вопросов. Хотите заполнить анкету сейчас?', reply_markup=markup)
-    bot.register_next_step_handler(message, lambda messege: questionnaire(messege, bot))
+    bot.register_next_step_handler(message, lambda message: questionnaire(message, bot))
